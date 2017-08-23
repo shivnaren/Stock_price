789455a7f57d420a0c317d4bdb94b38a0a110c80
shivnaren <sivanarayana93@gmail.com>
2017-08-23 07:52:23 +0530

    Update Stocks1.py

--- Stocks1.py
+++ Stocks1.py
@@ -3,6 +3,8 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import os.path
+import Stocks_Price
+import Stock_trading_Check
 i=0
 raw_comp_links_list = []
 #this is url to find out all prices
@@ -14,7 +16,7 @@ def comp_links(alpha_url_para):
     for link_table in table.find_all('tr')[1:]:
         for link_data in link_table.find_all('a'):
             raw_comp_links_list.append(link_data['href'])
-            print link_data['href']
+            #print link_data['href']
 
 alphasymobls = [u'A', u'B', u'C', u'D', u'E', u'F', u'G', u'H', u'I', u'J', u'K', u'L', u'M', u'N', u'O', u'P', u'Q', u'R', u'S', u'T', u'U', u'V', u'W', u'X', u'Y', u'Z', u'others']
 while i< len(alphasymobls):
@@ -22,24 +24,32 @@ while i< len(alphasymobls):
     comp_links(alpha_url)
     i = i + 1
 
-print len(raw_comp_links_list)
-comp_links_list = [x for x in raw_comp_links_list if x != ""]
-print len(comp_links_list)
+#Removing empty links
+All_comp_links_list = [x for x in raw_comp_links_list if x != ""]
 
 
-def stock_price(comp_url):
-    url = comp_url
-    htmlfile = requests.get(url)
-    htmltext = htmlfile.text
-    soup = BeautifulSoup(htmltext,'html.parser')
-    structured_soup = soup.prettify()
-    return soup.find('div',attrs = {"class":"FL PR5 rD_30"}).find('strong').string
-stock_price_data = {}
-for i in range(len(comp_links_list)):
-    price = stock_price(comp_links_list[i])
+#Check whether company is trading or not
+Trading_comps_links_list = []
+Not_Trading_comps_links_list = []
+for link_row in range(len(All_comp_links_list)):
+    if Stock_trading_Check.Stock_Trading_Check(All_comp_links_list[link_row]) != " ":
+        Trading_comps_links_list.append(All_comp_links_list[link_row])
+    else:
+        Not_Trading_comps_links_list.append(All_comp_links_list[link_row])
+
+print All_comp_links_list, len(All_comp_links_list) , Not_Trading_comps_links_list, len(Not_Trading_comps_links_list)
 
 
-    print stock_price(comp_url)
+
+
+
+
+
+
+
+
+
+
 
 
 
