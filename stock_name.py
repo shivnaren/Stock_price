7d18424bae9c1c875dcdc4d837e471887ffb9249
shivnaren <sivanarayana93@gmail.com>
2017-08-23 07:53:41 +0530

    Update stock_name.py

--- stock_name.py
+++ stock_name.py
@@ -2,13 +2,12 @@ import requests
 import urllib
 from bs4 import BeautifulSoup
 comp_url = "http://www.moneycontrol.com/india/stockpricequote/mining-minerals/20microns/2M"
-def stock_code(comp_url):
+def stock_price(comp_url):
     url = comp_url
     htmlfile = requests.get(url)
     htmltext = htmlfile.text
     soup = BeautifulSoup(htmltext,'html.parser')
-    #structured_soup = soup.prettify()
-    data = soup.find('div',attrs = {"class":"FL gry10"}).text.encode('utf-8')
-    for pipe in range(len(data)):
-        decode('utf-8')
-stock_code(comp_url)
+    structured_soup = soup.prettify()
+    return soup.find('div',attrs = {"class":"FL PR5 rD_30"}).find('strong').string
+
+
