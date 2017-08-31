b27ab96b9e00698891fcda31f4be9ae8b33292ec
shivnaren <sivanarayana93@gmail.com>
2017-08-31 19:21:52 +0530

    Update Stock_trading_Check.py

--- Stock_trading_Check.py
+++ Stock_trading_Check.py
@@ -17,26 +17,21 @@ def Stock_Trading_Check(comp_url):
 
 
 
-trading_list = []
-Not_trading_list = []
+trading_in_BSE_NSE = []
+trading_in_one = []
+Not_trading_in_BSE_NSE = []
 fp = open("links.txt")
 for line in enumerate(fp):
     if line[1] != "" and line[1] != 0:
         data = Stock_Trading_Check(line[1])
+        if len(data) == 2:
+            Not_trading_in_BSE_NSE.append(line[1])
+        elif len(data) == 1:
+            trading_in_one.append(line[1])
+        else:
+            trading_in_BSE_NSE = [line[1]]
 
-    if not data[0] in "is not traded on BSE in the last 30 days" and not data[0] in "is not traded":
-        print "starting Not trading list"
-        print line
-        print data
-        
-        print "ENDING Not trading list"
-        Not_trading_list.append(line)
-    else:
-        print "starting trading list"
-        print line
-        print data
-        print "ENDING trading list"
-        trading_list.append(line)
 
-print len(trading_list)
-print len(Not_trading_list)
+print len(Not_trading_in_BSE_NSE)
+print len(trading_in_one)
+print len(trading_in_BSE_NSE)
